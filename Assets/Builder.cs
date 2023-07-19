6516230eb6a8fc3fdc176f4db6327a6c24904733
Antonio <Antonio0913>
2023-07-19 15:12:12 +0800

    first commit

---
+++ Assets/Builder.cs
@@ -0,0 +1,24 @@
+﻿using UnityEditor;
+class Builder
+{
+    static void AndroidBuilder()
+    {
+        EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTargetGroup.Android, BuildTarget.Android);
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+        buildPlayerOptions.scenes = new[] { "Assets/Scenes/Scene1.unity"};
+        buildPlayerOptions.locationPathName = "AndroidBuild";
+        buildPlayerOptions.target = BuildTarget.Android;
+        buildPlayerOptions.options = BuildOptions.None;
+        BuildPipeline.BuildPlayer(buildPlayerOptions);
+    }
+    static void WindowsBuilder()
+    {
+        EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTargetGroup.Standalone, BuildTarget.StandaloneWindows);
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+        buildPlayerOptions.scenes = new[] { "Assets/Scenes/Scene1.unity" };
+        buildPlayerOptions.locationPathName = "WindowsBuild";
+        buildPlayerOptions.target = BuildTarget.StandaloneWindows;
+        buildPlayerOptions.options = BuildOptions.None;
+        BuildPipeline.BuildPlayer(buildPlayerOptions);
+    }
+}
